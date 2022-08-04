5b26cf8c598a01f95e4dde8b65474ad5afe53971
Ruben Andino <rubenanlo@gmail.com>
2022-08-04 23:31:43 +0200

    fixed typos

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,6 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/plan-that-feeding-server";
+require("dotenv/config");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/plan-that-meal-server";
 
 module.exports = MONGO_URI;

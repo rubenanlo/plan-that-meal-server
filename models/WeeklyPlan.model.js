2add96b108c5842e4be5267d2c8749e1488575d0
Ruben Andino <rubenanlo@gmail.com>
2022-08-07 19:26:14 +0200

    modified weekly plannings model and routes

--- models/WeeklyPlan.model.js
+++ models/WeeklyPlan.model.js
@@ -7,16 +7,28 @@ const weeklyplanSchema = new Schema(
       min: "2020-12-27",
       default: () => Date.now(),
     },
-    mealType: {
-      type: String,
-      enum: ["Breakfast", "Lunch", "Dinner"],
-    },
-    weeklyRecipes: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Recipe",
-      },
-    ],
+
+    mealRecipe11: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe12: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe21: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe22: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe31: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe32: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe41: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe42: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe51: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe52: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe61: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe62: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe71: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    mealRecipe72: { type: Schema.Types.ObjectId, ref: "Recipe" },
+
+    // dailyPlan: {
+    //   type: Array,
+    //   meal: {
+    //     mealRecipe: { type: Schema.Types.ObjectId, ref: "Recipe" },
+    //   },
+    // },
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
--- routes/weeklyPlan.routes.js
+++ routes/weeklyPlan.routes.js
@@ -11,7 +11,20 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 router.get("/weeklyplans", isAuthenticated, (req, res, next) => {
   WeeklyPlan.find()
     .populate("user")
-    .populate("weeklyRecipes")
+    .populate("mealRecipe11")
+    .populate("mealRecipe12")
+    .populate("mealRecipe21")
+    .populate("mealRecipe22")
+    .populate("mealRecipe31")
+    .populate("mealRecipe32")
+    .populate("mealRecipe41")
+    .populate("mealRecipe42")
+    .populate("mealRecipe51")
+    .populate("mealRecipe52")
+    .populate("mealRecipe61")
+    .populate("mealRecipe62")
+    .populate("mealRecipe71")
+    .populate("mealRecipe72")
     .then((allWeeklyPlans) => {
       const userWeeklyPlan = allWeeklyPlans.filter(
         (weeklyPlan) => weeklyPlan.user._id == req.payload._id
@@ -25,11 +38,112 @@ router.get("/weeklyplans", isAuthenticated, (req, res, next) => {
 router.post("/weeklyplans", isAuthenticated, (req, res, next) => {
   WeeklyPlan.create({
     startDate: req.body.startDate,
-    mealType: req.body.mealType,
-    weeklyRecipes: [],
+    mealRecipe11: req.body.mealRecipe11,
+    mealRecipe12: req.body.mealRecipe12,
+    mealRecipe21: req.body.mealRecipe21,
+    mealRecipe22: req.body.mealRecipe22,
+    mealRecipe31: req.body.mealRecipe31,
+    mealRecipe32: req.body.mealRecipe32,
+    mealRecipe41: req.body.mealRecipe41,
+    mealRecipe42: req.body.mealRecipe42,
+    mealRecipe51: req.body.mealRecipe51,
+    mealRecipe52: req.body.mealRecipe52,
+    mealRecipe61: req.body.mealRecipe61,
+    mealRecipe62: req.body.mealRecipe62,
+    mealRecipe71: req.body.mealRecipe71,
+    mealRecipe72: req.body.mealRecipe72,
     user: req.payload._id,
   })
-    .then((response) => res.json(response))
+    .then((newMealPlan) => {
+      console.log(newMealPlan);
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe11, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....11");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe12, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....12");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe21, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....21");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe22, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....22");
+
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe31, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....31");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe32, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....32");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe41, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....41");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe42, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....42");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe51, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....51");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe52, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....52");
+
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe61, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....61");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe62, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....62");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe71, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then((newMealPlan) => {
+      console.log("this is the number.....71");
+      return Recipe.findByIdAndUpdate(newMealPlan.mealRecipe72, {
+        $push: { weeklyPlan: newMealPlan._id },
+      });
+    })
+    .then(
+      console.log("this is the number.....72")
+      // res.json(response)
+    )
     .catch((err) => res.json(err));
 });
 
@@ -43,7 +157,20 @@ router.get("/weeklyplans/:weeklyPlanId", isAuthenticated, (req, res, next) => {
   }
 
   WeeklyPlan.findById(weeklyPlanId)
-    .populate("weeklyRecipes")
+    .populate("mealRecipe11")
+    .populate("mealRecipe12")
+    .populate("mealRecipe21")
+    .populate("mealRecipe22")
+    .populate("mealRecipe31")
+    .populate("mealRecipe32")
+    .populate("mealRecipe41")
+    .populate("mealRecipe42")
+    .populate("mealRecipe51")
+    .populate("mealRecipe52")
+    .populate("mealRecipe61")
+    .populate("mealRecipe62")
+    .populate("mealRecipe71")
+    .populate("mealRecipe72")
     .then((recipe) => res.json(recipe))
     .catch((error) => res.json(error));
 });

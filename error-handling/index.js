5d507b01e3aea4f0ff5c7bc3a44767cbff073021
Ruben Andino <rubenanlo@gmail.com>
2022-08-09 14:53:33 +0200

    changed error message

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).json({ errorMessage: "This route does not exist" });
+    res.status(404).json({ errorMessage: "This route does not exist." });
   });
 
   app.use((err, req, res, next) => {
